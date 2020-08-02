18edb24a3515cefc8cba905c857bf35b8621f258
dduck00 <ejr075@gmail.com>
2020-08-02 20:32:09 +0900

    ee

--- src/main/kotlin/com/example/HelloServer.kt
+++ src/main/kotlin/com/example/HelloServer.kt
@@ -1,3 +1,16 @@
+package com.example
+
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
 fun main() {
+}
+
+
+class HelloServer constructor(private val port:Int){
+    val server: Server = ServerBuilder
+        .forPort(port)
+        .build()
+
 
 }
